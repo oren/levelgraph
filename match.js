83d190fe55b2c9292872f0830ec55f206fb35073
Oren <orengolan@gmail.com>
2015-07-11 13:02:17 -0700

    pay and match

---
+++ match.js
@@ -0,0 +1,21 @@
+var level = require("level-browserify");
+var levelgraph = require("levelgraph");
+
+var db = levelgraph(level("payment-PA"));
+
+var distances = [
+  {subject: "armando-112", predicate: "distance", object: "josh-939", "miles": 11},
+  {subject: "armando-112", predicate: "distance", object: "mali-940" , "miles": 13}
+];
+
+db.put(distances, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
+
+var stream = db.getStream({subject: "armando-112"});
+stream.on("data", function(data) {
+    console.log('data', data);
+});
--- pay.js
+++ pay.js
@@ -2,27 +2,23 @@ var level = require("level-browserify");
 var levelgraph = require("levelgraph");
 
 // just use this in the browser with the provided bundle
-var db = levelgraph(level("payment-PA"));
+var db = levelgraph(level("payment"));
 
-var triplets = [
+var payments = [
   { subject: "armando-112", predicate: "pay-july", object: "350" },
   { subject: "armando-112", predicate: "pay-jun", object: "230" }
 ];
 
-db.put(triplets, function(err) {
+db.put(payments, function(err) {
   if (err) {
     console.log(err);
     return;
   }
 });
 
-// var stream = db.getStream({subject: "armando-112", predicate: "pay-july" });
-// var stream = db.getStream({predicate: "pay-july"});
 var stream = db.getStream({subject: "armando-112"});
 stream.on("data", function(data) {
     console.log('data', data);
 });
-// db.get({ subject: "armando-112" }, function(err, list) {
-//     console.log(list);
-// });
+
 
